135edc47829af4054fb1e2656f3cb884b75034a2
jellebee <jelle.hoekstra@hotmail.com>
2023-02-02 14:40:21 +0100

    Create Get_AAD_Group_Owner_Compare.ps1
    Created a check which users of Admin team exist as owners in other groups.
    Output will be a list of groups.

---
+++ Azure/Get_AAD_Group_Owner_Compare.ps1
@@ -0,0 +1,17 @@
+﻿#Connect To azureAD
+Connect-AzureAD
+#Set a list of groups
+$Grouplist = Get-AzureADGroup -Top 10000 | select *
+#Set cloudops group
+$cloudopsgroup = Get-AzADGroup -SearchString "AdminTeam"
+$cloudopsgroupowner = Get-AzureADGroupOwner -ObjectId $cloudopsgroup.id
+#Set group list
+$output = foreach($group in $Grouplist){
+    $groupusers = Get-AzureADGroupOwner -ObjectId $group.objectid
+    foreach($user in $groupusers){
+        if ($user.UserPrincipalName -in ($cloudopsgroupowner.UserPrincipalName)){
+            ($group.DisplayName)
+        }
+    }
+}
+$output | Select-Object -Unique | Sort-Object
